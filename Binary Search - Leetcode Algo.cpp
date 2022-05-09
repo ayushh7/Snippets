25f1642bea8c433c53a360fa06f7c9e253dffff6
Ayush Singh <btech10492.20@bitmesra.ac.in>
2022-05-09 15:21:58 +0530

    Algorithm Leetcode

---
+++ Binary Search - Leetcode Algo.cpp
@@ -0,0 +1,57 @@
+
+704. Binary Search
+Easy
+
+
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+Approach 1:
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        if(it==nums.end()||*it!=target)return -1;
+        else return it-nums.begin();
+    
+    }
+};
+
+
+Approach 2:
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      
+        int start=0,end=nums.size()-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(target<nums[mid]){
+                end=mid-1;
+            }else if(target>nums[mid])
+            {
+                start=mid+1;
+            }else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
